7473ec812fa4504b4e4d9e2635f305689fae7b5b
DESKTOP-ILDHSHN\KGA_11 <im5820795@naver.com>
2022-12-26 20:41:21 +0900

    [Junil]221226_4

---
+++ _39기_보강_1일차/Program.cs
@@ -0,0 +1,140 @@
+﻿
+using System;
+using System.Runtime.CompilerServices;
+// 그냥 메모
+/**
+ * 이렇게
+ * 여러 줄을
+ * 쓸 수 있다.
+ */
+
+
+namespace _39기_보강_1일차
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Main()은 프로그램의 시작점
+
+            //Console.WriteLine("Hello, World!");
+            //Console.Write("Hello, World2");
+            //Console.WriteLine("Hello, World3");
+            //Console.WriteLine("Hello, World4");
+            //Console.WriteLine("Hello, World5");
+
+            //Console.WriteLine("내이름은\"임준일\"입니다.");
+            //// \" : 문자라고 인식 시킴
+            //Console.WriteLine("내이름은\t임준일\t입니다.");
+
+            //Console.WriteLine("여기까지그냥적고게임할시간이");
+            //Console.WriteLine("여기까지그냥적고\r게임할시간이");
+            // \r : 여기부터 앞으로 돌아가서 앞 문자를 지우고 출력됨
+
+            //Console.WriteLine("다음 \n줄로 \n가요!\n");
+
+            //Console.WriteLine("다음 줄로 가요!");
+            //Console.WriteLine("다음 {0} 가요, {1} {0}", "상당히 긴 줄로", 11);
+
+
+
+            // 이스케이프 시퀀스가 뭐가 있는지 살펴보면 좋다.
+
+
+            // 나이, 이름, 성별 3가지를 자리표시자 출력 방법을 활용하여 작성
+            // 그리고 그 다음 줄에, [나는 다음 내용을 배울 "준비" 되었다]라고 출력
+
+            //Console.WriteLine("제 나이는 {0}이고, 이름은 {1}입니다. {2} 입니다.", 25, "임준일", "남자");
+            //Console.WriteLine("저는 다음 내용을 배울 \"준비\" 되었습니다.");
+
+
+            // 변수
+            // 정수형을 저장시키고 싶다.
+            //int intBox;
+            //int intBox2;
+
+
+            ////정수형 4를 저장하고 싶다.
+            ////intBox = 4;
+            //intBox2 = 5;
+
+            //int intBox = 5;
+
+            //int age = 14;
+
+            //age = 15; // 덮어씌우기 때문에 13은 의미가 없다.
+
+            //const int PhoneNumber = 010;
+            //PhoneNumber = 15; const의 값은 변경할 수 없다.
+            //const float PI = 3.14f;
+
+
+            //short shortNum = 11111;
+            //int intNum = 32769;
+
+            //shortNum = (short)intNum;
+
+            //short shortNum = 456;
+
+            //float floatNum = 15.59645f;
+
+            //floatNum = shortNum;
+
+            // (short) : short으로 바꿔서 들어가라
+            // 캐스팅 : 형을 강제로 바꾸는 것
+            //shortNum = (short)floatNum;
+
+            //Console.WriteLine(shortNum);
+
+
+            //long asd = 222222222222222222;
+            //float
+            //double
+
+            //c# 자료형 검색해서 보기
+
+            // bool : 참과 거짓을 판단하는 변수
+            //bool isTrue;
+            //isTrue = false;
+
+            //Console.WriteLine(" 긴 문자열 ");
+            //string 문자열 = "긴 문자열";
+            //char 캐릭터형 = 'a';
+
+
+            // 연산자
+
+            int firstNum = 8;
+            int secondNum = 3;
+
+
+            // 사칙연산
+            //firstNum = (firstNum + secondNum);
+            //firstNum = (firstNum - secondNum);
+
+            // 곱셈 결과의 값 범위 생각잘하기
+            //firstNum = (firstNum * secondNum);
+
+
+            // 나눗셈
+            // double 이용해서 소수점 표현
+            //double firstNum = 8;
+
+            //firstNum = (firstNum / secondNum);
+            //Console.WriteLine(firstNum);
+
+
+            // 나머지 값 표기
+            //firstNum = (firstNum % secondNum);
+            //Console.WriteLine(firstNum);
+
+            // 나머지 값이 0이면 실행해라~ 
+            //if(firstNum % secondNum == 0)
+            //{  }
+
+
+
+        }
+    }
+}
+
